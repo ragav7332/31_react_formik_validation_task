6106b6bf638364086f79906d61173db36bac73db
ragav7332 <ragav7332@gmail.com>
2023-12-02 09:39:31 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BooksContextProvider from './context/books.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
+    <BooksContextProvider>
     <App />
-  </React.StrictMode>
+    </BooksContextProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
